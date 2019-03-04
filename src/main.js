0ac6e0f570b14fa6433681c8ec374ee74f8394aa
lifenglu <fengluhome@163.com>
2019-03-04 11:31:50 +0800

    添加版本

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
  import pageHeader from './components/page-header/index';
-
+ let version = VERSION;
+ console.log(`TDUI v${version}`);
  const TDUI = {
      pageHeader,
+     version
  };
  export {
      pageHeader,
+     version
  };
  export default TDUI;
